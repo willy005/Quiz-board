8912c108e5ae4c5d0d4991b5bf4c8b8473ec8fc9
nillyw31@gmail.com <nillyw31@gmail.com>
2019-02-21 12:16:11 +0300

    Add all changes made!

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function check() {
     var questionOne = document.Quiz.questionOne.value;
-    var right = 5;
+    var right = 0;
 
 
     if (questionOne == "correct") {
-        right = right + 15;
+        right = right + 20;
     };
     document.getElementById("Results").style.visibility = "visible";
     document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "%";
